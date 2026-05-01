380f05a300f5231a797bf52e8d155486a28087e7
Diego Flores Prudente <diegofloresmax@gmail.com>
2026-04-30 18:50:28 -0600

    Corrige argumentos de start.py para Render

--- start.py
+++ start.py
@@ -2,16 +2,15 @@ import os
 import subprocess
 import sys
 
-# Obtenemos el puerto que nos da Render
+# Obtenemos el puerto que nos da Render (por defecto 8000 si no lo encuentra)
 port = os.environ.get("PORT", "8000")
 
-# Comando para forzar el arranque de Mercury
-# Usamos el ejecutable directo de python para evitar errores de PATH
+# Comando corregido para Mercury
 cmd = [
     sys.executable, "-m", "mercury", "run", 
-    "0.0.0.0:" + port, 
-    "--port", port,
-    "02_articulo_interactivo.ipynb"
+    "02_articulo_interactivo.ipynb", # El archivo que vas a servir
+    "--host", "0.0.0.0",             # CRÍTICO: Render necesita 0.0.0.0 para exponer la app a internet
+    "--port", port                   # El puerto dinámico de Render
 ]
 
 print(f"🚀 Arrancando Mercury en el puerto {port}...")
